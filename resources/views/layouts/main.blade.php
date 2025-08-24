4ba514d4daa8d5c238cec27e6ff68b765c6de6f9
mhdhafin <dhafin1147@gmail.com>
2025-08-24 11:02:00 +0700

    npm run build

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kantin Dharma Wanita | @yield('title')</title>
-    @vite('resources/css/app.css')
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="{{ asset('assets/js/darkmode.js') }}"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
